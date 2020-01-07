ae45e91da3b2fd0dd7805f1920f2d4630c07ea86
JeongHwa <rhyun9584@naver.com>
2020-01-07 21:35:36 +0900

    add MapBase

---
+++ Assets/Scripts/MapBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapBase : MonoBehaviour
+{
+    [SerializeField]
+    private MapCode mapCode;
+    [SerializeField]
+    private string mapName;
+
+
+}
