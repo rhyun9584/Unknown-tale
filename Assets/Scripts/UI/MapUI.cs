f36565b15541d8c1745ca305927846205174480e
JeongHwa <rhyun9584@naver.com>
2020-01-07 21:36:53 +0900

    add MapUI (OnOff)

---
+++ Assets/Scripts/UI/MapUI.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapUI : MonoBehaviour
+{
+    public static MapUI inst;
+
+    public GameObject showButton;
+
+    [Header("map UI")]
+    public GameObject mapUI;
+    public GameObject exitButton, moveButton, background;
+
+    private bool isActive;
+
+    void Awake()
+    {
+        if (inst == null)
+            inst = this;
+
+        isActive = true;
+        CloseMapUI();
+    }
+
+    public void OpenMapUI()
+    {
+        if (!isActive)
+        {
+            isActive = !isActive;
+
+            showButton.SetActive(!isActive);
+            mapUI.SetActive(isActive);
+        }
+    }
+
+    public void CloseMapUI()
+    {
+        if (isActive)
+        {
+            isActive = !isActive;
+
+            showButton.SetActive(!isActive);
+            mapUI.SetActive(isActive);
+        }
+    }
+}
