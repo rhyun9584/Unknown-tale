985c9cdd85118b63a5c58d53163bb8660ba4d056
JeongHwa <rhyun9584@naver.com>
2020-02-01 15:32:02 +0900

    MapUI 내 DetailUI 구현 및 SimpleUI 함수 위치 수정
    MapUI 스크립트 내에 nextLocation 변수를 두어 MoveLocationButton 스크립트의 활용을 줄임

--- Assets/Scripts/UI/MoveLocationButton.cs
+++ Assets/Scripts/UI/MoveLocationButton.cs
@@ -20,28 +20,6 @@ public class MoveLocationButton : MonoBehaviour
     }
 
     /// <summary>
-    /// Map UI에서 사용하는 이동 버튼
-    /// </summary>
-    public void MoveLocationInMap()
-    {
-        // Debug.Log(GameManager.inst.ReturnState() + " " + LocationManager.inst.locationScript[(int)nextLocation].GetActive()
-        //     + " " + LocationManager.inst.location[(int)nextLocation].GetComponent<LocationBase>().GetActive());
-        if(GameManager.inst.ReturnLocation() == nextLocation) // current Location == next Location
-        {
-            Debug.Log(nextLocation.ToString() + " is here");
-        }
-        else if(GameManager.inst.ReturnState() == State.Map && LocationManager.inst.locationScript[(int)nextLocation].GetActive() == true)
-        {
-            GameManager.inst.ChangeLocation(nextLocation);
-            //MapUI.inst.CloseMapUI();
-            PhoneUI.inst.ClosePhoneUI();
-        }
-        else // next Location is not activated 한 번도 방문한 적 없는 장소로 이동 시에
-        {
-            Debug.Log(nextLocation.ToString() + " is not activated");
-        }
-    }
-    /// <summary>
     /// Map UI 내 Location Button의 동작을 담당하는 함수
     /// simple UI를 켬
     /// </summary>
@@ -50,15 +28,4 @@ public class MoveLocationButton : MonoBehaviour
         MapUI.inst.OnSimpleUI(nextLocation);
     }
 
-    /// <summary>
-    /// Map UI의 Simple UI를 켜는 경우 moveLocationButton의 nextLocation을 수정
-    /// State가 Map인 경우만 동작하도록 설정하여 다른 상황에서 이용 불가
-    /// </summary>
-    public void ChangeNextLocation(LocationCode locationCode)
-    {
-        if(GameManager.inst.ReturnState() == State.Map)
-        {
-            this.nextLocation = locationCode;
-        }
-    }
 }
