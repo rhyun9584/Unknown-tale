307c7c1a61ec3750437ddfeabcade1cb5b3ec72a
JeongHwa <rhyun9584@naver.com>
2020-02-09 04:10:36 +0900

    Clue 획득 UI 구현
    clue 버튼 클릭시 이미지 -> 스크립트 -> 단서획득알림 순서대로 클릭해서 넘어가도록 구현

---
+++ Assets/Scripts/ClueBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClueBase : MonoBehaviour
+{
+    public string clueName;
+
+    private bool isObtain = false;
+
+    public void Obtain()
+    {
+        if (!isObtain)
+        {
+            isObtain = true;
+
+            ClueManager.inst.ObtainClue(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/ClueManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClueManager : MonoBehaviour
+{
+    public static ClueManager inst;
+
+    public GameObject[] clues;
+    public GameObject obtainUI, obtainScript, obtainPopUp;
+
+    private void Awake()
+    {
+        inst = this;
+    }
+
+    /// <summary>
+    /// 단서 획득
+    /// 1. clue UI에 획득 정보 전달
+    /// 2. 획득하는 화면 표시
+    /// </summary>
+    /// <param name="Clue"></param>
+    public void ObtainClue(ClueBase clue)
+    {
+
+        StartCoroutine(OpenObtainUI());
+    }
+    
+    IEnumerator OpenObtainUI()
+    {
+        bool next = true;
+
+        obtainUI.SetActive(true);
+
+        for (int i = 0; i < 3;)
+        {
+            if (next)
+            {
+                switch (i)
+                {
+                    case 1:
+                        obtainScript.SetActive(true);
+                        break;
+                    case 2:
+                        obtainPopUp.SetActive(true);
+                        break;
+                }
+
+                next = false;
+            }
+            else if (!next && Input.GetMouseButtonUp(0))
+            {
+                next = true;
+                i++;
+            }
+
+            yield return null;
+        }
+
+        obtainUI.SetActive(false);
+        obtainScript.SetActive(false);
+        obtainPopUp.SetActive(false);
+
+    }
+}
