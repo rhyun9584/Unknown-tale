0206663f986540271ba770947b671610e04c7de7
JeongHwa <rhyun9584@naver.com>
2020-02-12 21:11:27 +0900

    Tutorial에 등장하는 NPC enum에 추가

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -7,7 +7,12 @@ public enum NPCCode
     MAIN,
     SAMPLE1,
     SAMPLE2,
-    SAMPLE3
+    SAMPLE3,
+    OceanKing,  // 4 용왕
+    Octopus,    // 5 문어 대왕
+    OceanSun,   // 6 용왕 아들
+    Angler,     // 7 아귀 대신
+    Anchovy     // 8 멸치 시종
 }
 
 public enum State
